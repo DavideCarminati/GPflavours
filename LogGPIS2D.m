8d795f93724186b90372731d17829676b9ef8fc9
DavideCarminati <davide.carminati@polito.it>
2021-11-23 09:54:56 +0100

    Added SE kernel to logGPIS, working better than TP

--- LogGPIS2D.m
+++ LogGPIS2D.m
@@ -4,10 +4,11 @@
 % if referred to a single point.
 clear
 noise = 1e-3;
-lambda = 30;
+lambda = 30/1;
 numTest = 50;
 wallPoints = 10;
-R = 5;
+% R = 5;
+R = 1; % length scale for RBF kernel
 circleRadius = 0.5;
 x = [2.5, 2.5] + circleRadius * [cos(-pi:pi/4:(pi-1e-3))', sin(-pi:pi/4:(pi-1e-3))'];
 x2 = [4, 1] + 1.25*circleRadius * [cos(-pi:pi/4:(pi-1e-3))', sin(-pi:pi/4:(pi-1e-3))'];
@@ -15,11 +16,11 @@ x2 = [4, 1] + 1.25*circleRadius * [cos(-pi:pi/4:(pi-1e-3))', sin(-pi:pi/4:(pi-1e
 % x = [ x; x2 ];
 
 % Square room with circle obstacle:
-% x = [ x;
-%       linspace(0.6, 4.4, wallPoints)', 0.5*ones(wallPoints,1);
-%       linspace(0.6, 4.4, wallPoints)', 4.5*ones(wallPoints,1);
-%       0.5*ones(wallPoints,1), linspace(0.6, 4.4, wallPoints)';
-%       4.5*ones(wallPoints,1), linspace(0.6, 4.4, wallPoints)' ];
+x = [ x;
+      linspace(0.6, 4.4, wallPoints)', 0.5*ones(wallPoints,1);
+      linspace(0.6, 4.4, wallPoints)', 4.5*ones(wallPoints,1);
+      0.5*ones(wallPoints,1), linspace(0.6, 4.4, wallPoints)';
+      4.5*ones(wallPoints,1), linspace(0.6, 4.4, wallPoints)' ]; % working well with rbf kernel
 
 % x = [ x;
 %       0, 2;
@@ -34,14 +35,17 @@ x2 = [4, 1] + 1.25*circleRadius * [cos(-pi:pi/4:(pi-1e-3))', sin(-pi:pi/4:(pi-1e
 [ X1, X2 ] = meshgrid(linspace(0,5,numTest), linspace(0,5,numTest));
 X = [ X1(:), X2(:) ];
 
-[ K_tilde, K ] = kernelFnct2D(x, x, R);
-[ Ks_tilde, Ks ] = kernelFnct2D(X, x, R);
+% [ K_tilde, K ] = kernelFnct2D(x, x, R, 'ThinPlate');
+% [ Ks_tilde, Ks ] = kernelFnct2D(X, x, R, 'ThinPlate');
+
+[ K_tilde, K ] = kernelFnct2D(x, x, R, 'RBF');
+[ Ks_tilde, Ks ] = kernelFnct2D(X, x, R, 'RBF');
 
 y = zeros(size(x, 1), 1);
 % y = [ 0; 0; 0; 0; 0; 0; 0; 0 ];
 
-dy = [ 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; % Gradient along x1
-        -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2 ]; % Gradient along x2
+% dy = [ 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; % Gradient along x1
+%         -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2 ]; % Gradient along x2
 
 % dy = [ 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; % Gradient along x1
 %         1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; 0; sqrt(2)/2 ]; % Gradient along x2
@@ -49,11 +53,18 @@ dy = [ 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; % Gradient alo
 % dy = [ -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2;
 %         0; -sqrt(2)/2; -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2 ];
 
+dy = -[ -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2;
+        0; -sqrt(2)/2; -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2 ]; % Normal to obstacle border
+
 % dy = [ dy(1:length(dy)/2); dy(1:length(dy)/2); dy(length(dy)/2+1:end); dy(length(dy)/2+1:end) ];
 
 % Square room with circle obstacle:
 % dy = [ 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; -ones(wallPoints,1); ones(wallPoints,1); zeros(2*wallPoints,1);
 %         -1; -sqrt(2)/2; 0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; zeros(2*wallPoints,1); ones(wallPoints,1); -ones(wallPoints,1)]; 
+
+% Square room with circle obstacle - normal to obst
+dy = [ 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; 0; sqrt(2)/2; zeros(2*wallPoints,1); -ones(wallPoints,1); ones(wallPoints,1);
+        0; sqrt(2)/2; 1; sqrt(2)/2; 0; -sqrt(2)/2; -1; -sqrt(2)/2; -ones(wallPoints,1); ones(wallPoints,1); zeros(2*wallPoints,1) ]; 
 
 y = exp(-y*lambda);% + noise*randn(size(x, 1), 1);
 mu_g = Ks_tilde / K_tilde * [y; dy];
@@ -75,7 +86,7 @@ doublingIndex = floor(1:0.5:numTest^2+0.5); % creating index vector looking like
 % grad = -mu_g(numTest^2+1:end);
 
 normlz = sqrt(mu_g(numTest^2+1:2*numTest^2).^2 + mu_g(2*numTest^2+1:end).^2);
-grad = -mu_g(numTest^2+1:end)./normlz(doublingIndex);
+grad = -mu_g(numTest^2+1:end)./(normlz(doublingIndex)*lambda);
 
 % Rotate the gradients so that they are normal to surfaces
 theta = -pi/2;
@@ -83,18 +94,19 @@ rot = [ cos(theta)*eye(numTest^2), sin(theta)*eye(numTest^2); ...
         -sin(theta)*eye(numTest^2), cos(theta)*eye(numTest^2) ];
 grad = rot*grad;
 
-theta = pi/2;
+theta = 0*-pi/2;
 rot = [ cos(theta)*eye(length(dy)/2), sin(theta)*eye(length(dy)/2); ...
         -sin(theta)*eye(length(dy)/2), cos(theta)*eye(length(dy)/2) ];
 dy_normal = rot*dy;
 
 figure
-hsurf = surface(X1, X2, reshape(dist, numTest, numTest) - max(dist), 'FaceColor','interp','EdgeColor','interp');
+hsurf = surface(X1, X2, reshape(dist, numTest, numTest) - 0*max(dist), 'FaceColor','interp','EdgeColor','interp');
 % colormap gray;
 % imagesc(linspace(0,5,numTest), linspace(0,5,numTest), reshape(dist, numTest, numTest));
 hold on
 plot(x(:,1), x(:,2), '.','markersize',28,'color',[.7 0.3 0]); %Interior points
-quiver(x(:,1), x(:,2), dy_normal(1:length(y)), dy_normal(length(y)+1:end), 'g');
+quiver(x(:,1), x(:,2), dy_normal(1:length(y)), dy_normal(length(y)+1:end), 0.5 , 'g');
+contour(X1, X2, reshape(dist, numTest, numTest), [0,0], 'w');
 % quiver(X(:,1), X(:,2), grad(1:numTest^2), grad(numTest^2+1:end), 'w');
 % set(gca, 'Layer', 'top')
 hsurf.Annotation.LegendInformation.IconDisplayStyle = 'off';
@@ -103,38 +115,56 @@ xlabel('x_1 [m]')
 ylabel('x_2 [m]')
 xlim([0, 5])
 ylim([0, 5])
+colorbar%('Ticks', [min(diag(ysd))-max(diag(ysd)), 0])
 % view(3)
 
-% figure
-% surface(X1, X2, reshape(dist2, numTest, numTest), 'FaceColor','interp','EdgeColor','interp');
-% hold on
-% plot(x(:,1), x(:,2), '.','markersize',28,'color',[.8 0 0]); %Interior points
-% xlabel('x_1 [m]')
-% ylabel('x_2 [m]')
-% title('Without grad info');
+figure
+surface(X1, X2, reshape((mu_g(1:numTest^2)), numTest, numTest), 'FaceColor','interp','EdgeColor','interp');
+hold on
+plot(x(:,1), x(:,2), '.','markersize',28,'color',[.8 0 0]); %Interior points
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+title('Classic GP');
 
-function [ K_tilde, K ] = kernelFnct2D(x1, x2, R)
+function [ K_tilde, K ] = kernelFnct2D(x1, x2, R, Kerneltype)
 
-    K = 2*abs(pdist2(x1, x2).^3) - 3*R*pdist2(x1, x2).^2 + R^3; % Basis function
-    % How to differenciate in nD kernels: 
-    % https://math.stackexchange.com/questions/84331/does-this-derivation-on-differentiating-the-euclidean-norm-make-sense
+    % Thin plate covariance function
+    if strcmp(Kerneltype, 'ThinPlate')
+        K = 2*abs(pdist2(x1, x2).^3) - 3*R*pdist2(x1, x2).^2 + R^3; % Basis function
+        % How to differenciate in nD kernels: 
+        % https://math.stackexchange.com/questions/84331/does-this-derivation-on-differentiating-the-euclidean-norm-make-sense
 
-    dKx1 = [ 6*pairwiseDiff(x1(:,1), x2(:,1)).*(pdist2(x1, x2) - R); 
-             6*pairwiseDiff(x1(:,2), x2(:,2)).*(pdist2(x1, x2) - R) ];
-%     dKx2 = -dKx1'; % For stationary kernels holds this property, but note
-%     that the transposition of a marix containing matrices is the
-%     transpose of submatrices inside the outer transposed matrix
-    dKx2 = [ (6*pairwiseDiff(x2(:,1), x1(:,1)).*(pdist2(x2, x1) - R))', ...
-             (6*pairwiseDiff(x2(:,2), x1(:,2)).*(pdist2(x2, x1) - R))' ];
-         
-    dKx2 = [ -(6*pairwiseDiff(x1(:,1), x2(:,1)).*(pdist2(x1, x2) - R)), ...
-             -(6*pairwiseDiff(x1(:,2), x2(:,2)).*(pdist2(x1, x2) - R)) ];
-         
-    ddK = [ -6*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,1), x2(:,1))./(pdist2(x1, x2)+1e-4) + pdist2(x1, x2) - R, ...
-            -6*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,2), x2(:,2))./(pdist2(x1, x2)+1e-4);
-            -6*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,1), x2(:,1))./(pdist2(x1, x2)+1e-4), ...
-            -6*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,2), x2(:,2))./(pdist2(x1, x2)+1e-4) + pdist2(x1, x2) - R ];
-    K_tilde = [ K, dKx2; dKx1, ddK ];
+        dKx1 = [ 6*pairwiseDiff(x1(:,1), x2(:,1)).*(pdist2(x1, x2) - R); 
+                 6*pairwiseDiff(x1(:,2), x2(:,2)).*(pdist2(x1, x2) - R) ];
+    %     dKx2 = -dKx1'; % For stationary kernels holds this property, but note
+    %     that the transposition of a marix containing matrices is the
+    %     transpose of submatrices inside the outer transposed matrix
+        dKx2 = [ (6*pairwiseDiff(x2(:,1), x1(:,1)).*(pdist2(x2, x1) - R))', ...
+                 (6*pairwiseDiff(x2(:,2), x1(:,2)).*(pdist2(x2, x1) - R))' ];
+
+        dKx2 = [ -(6*pairwiseDiff(x1(:,1), x2(:,1)).*(pdist2(x1, x2) - R)), ...
+                 -(6*pairwiseDiff(x1(:,2), x2(:,2)).*(pdist2(x1, x2) - R)) ];
+
+        ddK = [ -6*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,1), x2(:,1))./(pdist2(x1, x2)+1e-10) + pdist2(x1, x2) - R, ...
+                -6*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,2), x2(:,2))./(pdist2(x1, x2)+1e-10);
+                -6*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,1), x2(:,1))./(pdist2(x1, x2)+1e-10), ...
+                -6*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,2), x2(:,2))./(pdist2(x1, x2)+1e-10) + pdist2(x1, x2) - R ];
+        K_tilde = [ K, dKx2; dKx1, ddK ];
+    elseif strcmp(Kerneltype, 'RBF')
+        sigma = 1;
+        K = sigma*exp(-pdist2(x1, x2)/(2*R^2));
+        dKx1 = [ -1/R^2*pairwiseDiff(x1(:,1), x2(:,1)).*K;
+                 -1/R^2*pairwiseDiff(x1(:,2), x2(:,2)).*K ];
+        dKx2 = [ 1/R^2*pairwiseDiff(x1(:,1), x2(:,1)).*K, ...
+                 1/R^2*pairwiseDiff(x1(:,2), x2(:,2)).*K ];
+        ddK = [ (1/R^2 - 1/R^4*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,1), x2(:,1))).*K, ...
+                (-1/R^4*pairwiseDiff(x1(:,1), x2(:,1)).*pairwiseDiff(x1(:,2), x2(:,2))).*K; 
+                (-1/R^4*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,1), x2(:,1))).*K, ...
+                (1/R^2 - 1/R^4*pairwiseDiff(x1(:,2), x2(:,2)).*pairwiseDiff(x1(:,2), x2(:,2))).*K ];
+        K_tilde = [ K, dKx2; dKx1, ddK ];
+    else
+        error('Kernel type not recognized')
+    end
 end
 
 function out = pairwiseDiff(x1, x2)
